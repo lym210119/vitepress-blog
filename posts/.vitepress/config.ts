a5c25a41eef740c2c802de967fbf653fe8cf7253
liyangming <lym210119@gmail.com>
2023-09-06 18:37:55 +0800

    initial commit

---
+++ posts/.vitepress/config.ts
@@ -0,0 +1,37 @@
+import { defineConfig } from 'vitepress'
+
+// https://vitepress.dev/reference/site-config
+export default defineConfig({
+  title: "My Awesome Project",
+  description: "A VitePress Site",
+  themeConfig: {
+    // https://vitepress.dev/reference/default-theme-config
+    nav: [
+      // { text: 'Home', link: '/' },
+      { text: 'Blog', link: '/blog' },
+      { text: 'Tags', link: '/tags' },
+      { text: 'About', link: '/about' },
+      { text: 'Examples', link: '/markdown-examples' },
+    ],
+
+    sidebar: [
+      {
+        text: 'Blog',
+        items: [
+          { text: 'All Posts', link: '/blog/index' },
+        ]
+      },
+      {
+        text: 'Examples',
+        items: [
+          { text: 'Markdown Examples', link: '/markdown-examples' },
+          { text: 'Runtime API Examples', link: '/api-examples' }
+        ]
+      }
+    ],
+
+    socialLinks: [
+      { icon: 'github', link: 'https://github.com/vuejs/vitepress' }
+    ]
+  }
+})
