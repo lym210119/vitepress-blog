778b1ab7961aa7965c90dca3578853dfbd961fd9
liyangming <lym210119@gmail.com>
2023-10-09 16:18:21 +0800

    initial commit

--- blog/.vitepress/theme/components/HomePage.vue
+++ blog/.vitepress/theme/components/HomePage.vue
@@ -12,25 +12,7 @@ import { teamMembers } from '../../contributors'
             Meet The Team
           </h2>
           <div w-full p-10>
-            <VPTeamMembers size="small" :members="teamMembers" />
-          </div>
-          <h2 id="sponsored-by" op70 font-normal pt-5 pb-2>
-            Sponsored by
-          </h2>
-          <div grid="~ cols-1" w-full text-center mt-10>
-            <div class="flex flex-col">
-              <a href="https://cdn.jsdelivr.net/gh/antfu/static/sponsors.svg" target="_blank" rel="noopener noreferrer">
-                <img
-                  crossorigin="anonymous"
-                  inline-block
-                  width="768" height="1464"
-                  class="resizable-img"
-                  loading="lazy"
-                  src="https://cdn.jsdelivr.net/gh/antfu/static/sponsors.svg"
-                  alt="Anthony Fu's sponsors"
-                >
-              </a>
-            </div>
+            <!-- <VPTeamMembers size="small" :members="teamMembers" /> -->
           </div>
         </div>
       </main>
